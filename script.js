a8b8ad35404735af248942d74caa456e209d7fbb
HappyCodingMama <HappyCodingMama@gmail.com>
2020-08-17 18:56:50 +0900

    coding for adding tasks

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 const form = document.querySelector('#task-form');
-const ulCollect = document.querySelector('.collection');
+const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-loadEventListeners();
 
-function loadEventListeners() {
-    form.addEventListener('submit', addTask);
-}
+//call all event listeners
+loadAddEventListener();
 
-function addTask(e) {
+function loadAddEventListener() {
+
+form.addEventListener('submit', addTask);
+
+function addTask(event) {
     if (taskInput.value === '') {
         alert('add a task');
     }
 
-    const li = document.createElement('li');
+    const li = document.createElement('li');   
     li.className = 'collection-item';
     li.appendChild(document.createTextNode(taskInput.value));
 
     const link = document.createElement('a');
     link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
-
+    link.innerHTML = '<i class="fa fa-times"></i>';
     li.appendChild(link);
 
-    ulCollect.appendChild(li);
-
+    taskList.appendChild(li);
     taskInput.value = '';
 
-    e.preventDefault();
+    event.preventDefault();
+
 }
+}
+
+
+
+
+
+
 
 
 
